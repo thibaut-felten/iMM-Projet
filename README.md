7406099c9e73393d74f6a2ed7fcedc526002c976
Tibo <thib54700@hotmail.fr>
2020-03-30 15:28:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# iMM - Projet IDAW
+
+Application Web permettant de maintenir un journal de tous les aliments que vous consommez. Un tracker de nourriture permet de mieux contrôler ses différents apports énergétiques.
---
+++ src/script.js
@@ -0,0 +1,8 @@
+// Tous les scripts utiles au projet
+// Si trop long go faire plusieurs fichiers de script
+
+
+// Fonction à appeler pour avoir le tableau DataTables - Remplacer #myTable par le nom de notre tableau
+$(document).ready( function () {
+    $('#journal').DataTable();
+} );
