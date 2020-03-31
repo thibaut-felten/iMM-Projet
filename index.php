983996ed79ed24a106a683207f919a70c72dbd31
Tibo <thib54700@hotmail.fr>
2020-03-31 17:18:40 +0200

    Graphiques ajoutés

--- index.php
+++ index.php
@@ -25,17 +25,49 @@ require_once("src/head.php");
         </div>
     </nav>
     <div class="container-fluid mt-2">
-        <div id="cardJournal" class="card">
-            <div class="card-header">
-                Calories
+        <div class="row">
+            <div id="cardChartCalorie" class="card cardChart">
+                <div class="card-header">
+                    Qunantité de calories ingérées
+                </div>
+                <div class="card-body">
+                    <canvas id="chartCal"></canvas>
+                </div>
             </div>
-            <div class="card-body">
-                <canvas id="chartCal"></canvas>
+            <div id="cardChartSucre" class="card cardChart">
+                <div class="card-header">
+                    Quantité de sucre consommée
+                </div>
+                <div class="card-body">
+                    <canvas id="chartSucre"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div id="cardChartSel" class="card cardChart">
+                <div class="card-header">
+                    Quantité de sel ingérée
+                </div>
+                <div class="card-body">
+                    <canvas id="chartSel"></canvas>
+                </div>
+            </div>
+            <div id="cardChartTypeAliments" class="card cardChart">
+                <div class="card-header">
+                    Types d'aliments consommés
+                </div>
+                <div class="card-body">
+                    <canvas id="chartType"></canvas>
+                </div>
             </div>
         </div>
     </div>
 </body>
 
-<script>new Chart(document.getElementById("chartCal"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(75, 192, 192)","lineTension":0.1}]},"options":{}});</script>
+<script>new Chart(document.getElementById("chartCal"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(255, 99, 132)","lineTension":0.1}]},"options":{}});
+new Chart(document.getElementById("chartSel"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(54, 162, 235)","lineTension":0.1}]},"options":{}});
+new Chart(document.getElementById("chartSucre"),{"type":"line","data":{"labels":["January","February","March","April","May","June","July"],"datasets":[{"label":"My First Dataset","data":[65,59,80,81,56,55,40],"fill":false,"borderColor":"rgb(255, 205, 86)","lineTension":0.1}]},"options":{}});
+new Chart(document.getElementById("chartType"),{"type":"doughnut","data":{"labels":["Viande","Légumes/Fruits","Féculents"],"datasets":[{"label":"My First Dataset","data":[300,50,100],"backgroundColor":["rgb(255, 99, 132)","rgb(54, 162, 235)","rgb(255, 205, 86)"]}]}});
+</script>
 
 </html>
